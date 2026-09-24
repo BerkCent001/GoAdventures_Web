cfe98edfb64fe39d9d23028ddacd62fec264537a
BerkCent001 <klibergonza00@gmail.com>
2026-09-23 23:39:25 -0600

    Add files via upload

---
+++ tailwind-config.js
@@ -0,0 +1,16 @@
+// Configuración externa de Tailwind CSS
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'fondo-oscuro': '#0a0a0a',
+                'fondo-aventura': '#140a05',
+                'fondo-cultura': '#171206',
+                'fondo-natura': '#06140b',
+                'verde-natura': '#10b981',
+                'naranja-aventura': '#ea580c',
+                'oro-cultura': '#d97706'
+            }
+        }
+    }
+}
